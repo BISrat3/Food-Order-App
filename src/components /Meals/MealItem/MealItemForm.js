7ea649501eb4082d9e2143463a869d193d0c93ef
bisrat <bisrat.amtataw@gmail.com>
2022-10-21 11:40:31 -0400

    cart context

--- src/components /Meals/MealItem/MealItemForm.js
+++ src/components /Meals/MealItem/MealItemForm.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Input from '../../UI/Input'
 import classes from './MealItemForm.module.css'
+import { useRef } from 'react'
 
 export default function MealItemForm(props) {
+
+    const amountInputRef = useRef()
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+
+        const enteredAmountNumber = +enteredAmount
+
+        if(enteredAmount.trim().length === 0 || 
+            enteredAmountNumber <1 || 
+            enteredAmountNumber >5){
+                return;
+            }
+    }
+
   return (
-    <form className={classes.form}>
-        <Input label="Amount" input={{
+    <form className={classes.form} onSubmit={submitHandler}>
+        <Input 
+            ref={amountInputRef}
+            label="Amount" input={{
             id:'amount',
             type: 'number',
             min :'1',
