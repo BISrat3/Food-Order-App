e0e5cd47644deab41bd91822391c3c92a4ae2cef
bisrat <bisrat.amtataw@gmail.com>
2022-11-19 21:55:37 -0500

    connect with database

--- src/components /Meals/AvailableMeals.js
+++ src/components /Meals/AvailableMeals.js
@@ -3,32 +3,32 @@ import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name:'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id:'m2',
-        name:'Schbitzel',
-        description: 'A german speciality',
-        price:16.5,
-    },
-    {
-        id:'m3',
-        name:'Tibs',
-        description: 'Ethiopian Food',
-        price:18.5,
-    },
-    {
-        id:'m4',
-        name:'Barbecue Burger',
-        description: 'American, raw, meaty ',
-        price:17.5,
-    },
-]
+// const DUMMY_MEALS = [
+//     {
+//         id: 'm1',
+//         name:'Sushi',
+//         description: 'Finest fish and veggies',
+//         price: 22.99,
+//     },
+//     {
+//         id:'m2',
+//         name:'Schbitzel',
+//         description: 'A german speciality',
+//         price:16.5,
+//     },
+//     {
+//         id:'m3',
+//         name:'Tibs',
+//         description: 'Ethiopian Food',
+//         price:18.5,
+//     },
+//     {
+//         id:'m4',
+//         name:'Barbecue Burger',
+//         description: 'American, raw, meaty ',
+//         price:17.5,
+//     },
+// ]
 
 export default function AvailableMeals() {
     const mealsList= DUMMY_MEALS.map((meal)=> 
