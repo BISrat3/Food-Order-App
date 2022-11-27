09f98e0d576ebc9c901b2dcd31cc12f60d57b68f
bisrat <bisrat.amtataw@gmail.com>
2022-11-26 20:48:26 -0600

    update availability

--- src/components /Meals/AvailableMeals.js
+++ src/components /Meals/AvailableMeals.js
@@ -1,36 +1,42 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name:'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id:'m2',
-        name:'Schbitzel',
-        description: 'A german speciality',
-        price:16.5,
-    },
-    {
-        id:'m3',
-        name:'Tibs',
-        description: 'Ethiopian Food',
-        price:18.5,
-    },
-    {
-        id:'m4',
-        name:'Barbecue Burger',
-        description: 'American, raw, meaty ',
-        price:17.5,
-    },
-]
+// const DUMMY_MEALS = [
+//     {
+//         id: 'm1',
+//         name:'Sushi',
+//         description: 'Finest fish and veggies',
+//         price: 22.99,
+//     },
+//     {
+//         id:'m2',
+//         name:'Schbitzel',
+//         description: 'A german speciality',
+//         price:16.5,
+//     },
+//     {
+//         id:'m3',
+//         name:'Tibs',
+//         description: 'Ethiopian Food',
+//         price:18.5,
+//     },
+//     {
+//         id:'m4',
+//         name:'Barbecue Burger',
+//         description: 'American, raw, meaty ',
+//         price:17.5,
+//     },
+// ]
 
 export default function AvailableMeals() {
+
+    useEffect (()=>{
+
+        fetch()
+    },[])
+
     const mealsList= DUMMY_MEALS.map((meal)=> 
     <MealItem 
         key={meal.id} 
